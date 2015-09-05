2f6659321c15d03f8b960023abf5eed14386f3ed
brunopita <brunorogerior@gmail.com>
2015-09-05 18:44:31 -0300

    Removida as opções do menu administrador

--- wordpress/wp-content/themes/accesspress-ray/functions.php
+++ wordpress/wp-content/themes/accesspress-ray/functions.php
@@ -104,3 +104,30 @@ require get_template_directory() . '/inc/jetpack.php';
  * Load TGM_Plugin_Activation class.
  */
 require get_template_directory() . '/inc/class-tgm-plugin-activation.php';
+
+function pc_remove_submenus() {
+ 
+  global $submenu;
+ 
+  unset($submenu['themes.php'][5]); // Remove 'Temas'.
+  unset($submenu['options-general.php'][15]); // Remove 'Escrita'.
+  unset($submenu['options-general.php'][25]); // Remove 'Discussão'.
+  unset($submenu['tools.php'][5]); // Remove 'Disponíveis'.
+  unset($submenu['tools.php'][10]); // Remove 'Importar'.
+  unset($submenu['tools.php'][15]); // Remove 'Exportar'.
+}
+add_action( 'admin_menu', 'pc_remove_submenus' );
+
+function remove_editor_menu() {
+  remove_action('admin_menu', '_add_themes_utility_last', 101);
+}
+ 
+add_action('_admin_menu', 'remove_editor_menu', 1);
+ 
+// Remove Link Plugin > Editor
+ 
+function pc_remove_plugin_editor() {
+  remove_submenu_page( 'plugins.php', 'plugin-editor.php' );
+}
+ 
+add_action('admin_init', 'pc_remove_plugin_editor');
